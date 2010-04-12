213bc35f3ab47d38d3401bb8ea335682a077db17
Nicholas Malaya <nicholas.malaya@gmail.com>
2010-04-12 22:26:54 +0000

    finally getting the hang of this autoconf...took long enough

--- src/masa_shell.cpp
+++ src/masa_shell.cpp
@@ -1,10 +1,41 @@
+/*--------------------------------------------------------------------------
+  *--------------------------------------------------------------------------
+  *
+  * Copyright (C) 2010 The PECOS Development Team
+  *
+  * Please see http://pecos.ices.utexas.edu for more information.
+  *
+  * This file is part of MASA.
+  *
+  * MASA is free software: you can redistribute it and/or modify it under
+  * the terms of the GNU Lesser General Public License as published by the Free
+  * Software Foundation, either version 3 of the License, or (at your option)
+  * any later version.
+  *
+  * MASA is distributed in the hope that it will be useful, but WITHOUT ANY
+  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+  * details.
+  *
+  * You should have received a copy of the GNU Lesser General Public License along 
+  * with MASA.  If not, see <http://www.gnu.org/licenses/>.
+  *
+  *--------------------------------------------------------------------------
+  
+  MASA -- Manufactured Analytical Solutions Abstraction Library
+
+  A software interface that provides access to all manufactured solutions to 
+  be used by various models throughout the center.
+  
+  *--------------------------------------------------------------------------
+  */  
 
 // this is the MASA shell, for users to grow familiar with the masa namespace, as well as debugging
-// this probably should not be contained in the MASA namespace
 
 #include "masa.h"
 
 using namespace std;
+using namespace MASA;
 
 void masa_shell_choose_solution()
 {
@@ -14,8 +45,18 @@ void masa_shell_choose_solution()
   printf("\n Remember to place underscores between parameters (e.g. steady_heatequation)");
   printf("\n\n");
   scanf("%s",userstring);
-  masa_map_dimension(userstring,masterstring);  
+  masa_map_dimension(userstring,masterstring);
 }// end masa_shell_choose_solution
+
+void masa_shell_print_avail()
+{
+  // this function prints the available manufactured solutions -- 
+  // obviously needs to get a handle on the manufactured solutions class here
+  printf("\n");
+  printf("\n 2d heat equation");
+  printf("\n 3d heat equation");
+  printf("\n");
+}
 
 int main()
 {
@@ -51,7 +92,7 @@ int main()
 
 	case 1:
 	  printf("\n User Selected 1: Display Available Solutions");
-
+	  masa_shell_print_avail();
 	  break;
 	  
 	case 2:
