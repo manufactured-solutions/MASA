f0cc8230826baff7bc59a9ba89c1e9744ac3ce57
Nicholas Malaya <nicholas.malaya@gmail.com>
2010-08-13 23:02:52 +0000

    updating parser, not done

--- tests/maple/parser.cpp
+++ tests/maple/parser.cpp
@@ -2,11 +2,19 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <stdlib.h>
+#include <sstream>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+  int p,p2,dimension,numparam;
+  char ch;
+  string str,substr,substr2,substr3,name;
+  string comma = ","; 
+  //stringstream ss;
+
   // need one input argument
   if(argc != 2) 
     {
@@ -21,16 +29,23 @@ int main(int argc, char* argv[])
       exit(1);
     }
 
-  char ch;
-  string str;
-  //while(from.get(ch)) cout << ch;
-  while(!from.eof())
-    {
-      getline(from,str);
-      cout << str;
-	      //cout << ch;
-    }
+  // get first line
+  getline(from,str);
+  p = str.find(comma); //find first comma
+  stringstream ss(str.substr(0,p));
+  ss >> dimension;    
+  cout << dimension << endl;
+
+  p2 = str.find(comma,p+1); // 2nd comma
+  name = str.substr(p+1,p2-p-1);
+  cout << name << endl;
+  string start = str.substr(0, 5);  
+
+  cout << str.substr(p2+1) << endl;
+  ss << str.substr(p2+1,1); // final chunk
+  ss >> numparam;
+  cout << numparam << endl;
   
-  if(!from.eof()) cout << "something very weird has happened";
+  if(!from.eof()) cout << "something very weird has happened: file did not end!";
   
 }
