cddae4eafe62de6e90a6fce6de22f7e91089948d
Nicholas Malaya <nicholas.malaya@gmail.com>
2010-08-26 03:46:22 +0000

    [masa]: prepping for edits to license

--- examples/c_heat_example.c
+++ examples/c_heat_example.c
@@ -1,44 +1,34 @@
-// $License$
+// -*-c++-*-
+//
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// MASA - Manufactured Analytical Solutions Abstraction Library
+//
+// Copyright (C) 2010 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
 // $Author$
-// $Id$ 
-
-  /*--------------------------------------------------------------------------
-  *--------------------------------------------------------------------------
-  *
-  * Copyright (C) 2010 The PECOS Development Team
-  *
-  * Please see http://pecos.ices.utexas.edu for more information.
-  *
-  * This file is part of MASA.
-  *
-  * MASA is free software: you can redistribute it and/or modify it under
-  * the terms of the GNU Lesser General Public License as published by the Free
-  * Software Foundation, either version 3 of the License, or (at your option)
-  * any later version.
-  *
-  * MASA is distributed in the hope that it will be useful, but WITHOUT ANY
-  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-  * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-  * details.
-  *
-  * You should have received a copy of the GNU Lesser General Public License along 
-  * with MASA.  If not, see <http://www.gnu.org/licenses/>.
-  *
-  *--------------------------------------------------------------------------
-  
-  MASA -- Manufactured Analytical Solutions Abstraction Library
-
-  A software interface that provides access to all manufactured solutions to 
-  be used by various models throughout the center.
-  
-  *--------------------------------------------------------------------------
-  */  
-
-
-/* 
-  - this is an example of the heat equation in 1d for C-language -
-*/
-
+// $Id$
+//
+// c_heat_example.c: this is an example of the heat equation in 1d for C
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+//
 
 #include <cmasa.h>
 #include <stdio.h>
--- examples/masa_shell.cpp
+++ examples/masa_shell.cpp
@@ -1,40 +1,15 @@
-// $License$
+// -*-c++-*-
+//
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
 // $Author$
-// $Id$ 
+// $Id$
+//
+// masa_shell.cpp: for users to grow familiar with the masa namespace
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+//
 
-  /*--------------------------------------------------------------------------
-  *--------------------------------------------------------------------------
-  *
-  * Copyright (C) 2010 The PECOS Development Team
-  *
-  * Please see http://pecos.ices.utexas.edu for more information.
-  *
-  * This file is part of MASA.
-  *
-  * MASA is free software: you can redistribute it and/or modify it under
-  * the terms of the GNU Lesser General Public License as published by the Free
-  * Software Foundation, either version 3 of the License, or (at your option)
-  * any later version.
-  *
-  * MASA is distributed in the hope that it will be useful, but WITHOUT ANY
-  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-  * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-  * details.
-  *
-  * You should have received a copy of the GNU Lesser General Public License along 
-  * with MASA.  If not, see <http://www.gnu.org/licenses/>.
-  *
-  *--------------------------------------------------------------------------
-  
-  MASA -- Manufactured Analytical Solutions Abstraction Library
-
-  A software interface that provides access to all manufactured solutions to 
-  be used by various models throughout the center.
-  
-  *--------------------------------------------------------------------------
-  */  
-
-// this is the MASA shell, for users to grow familiar with the masa namespace, as well as debugging
 
 #include <masa.h>
 
