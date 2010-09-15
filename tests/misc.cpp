54b397d12c03398ad5aac882a4551eb344c21a36
Nicholas Malaya <nicholas.malaya@gmail.com>
2010-09-15 19:20:25 +0000

    [masa]: adding a misc.cpp reg test for various masa diagnostic and basic functions that need testing

---
+++ tests/misc.cpp
@@ -0,0 +1,53 @@
+// -*-c++-*-
+//
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// MASA - Manufactured Analytical Solutions Abstraction Library
+//
+// Copyright (C) 2010 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// $Author: nick $
+// $Id: c_euler1d.c 13404 2010-09-15 02:56:33Z nick $
+//
+// misc.cpp : program that tests masa helper functions
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#include <config.h>
+#include <masa.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace MASA;
+using namespace std;
+
+int main()
+{
+  int i;
+  masa_init("euler-test","euler_1d");
+  masa_get_dimension(&i);
+  
+  if(i==1)
+    return 0;
+  else
+    return 1;
+  
+}
