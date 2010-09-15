02d12d3ad5679ac28a82ddcb69767f8d21dceb42
Nicholas Malaya <nicholas.malaya@gmail.com>
2010-09-15 19:04:46 +0000

    [masa]: fiddling with gcov. Functions should be tested, but also mucks up buildbot output. Perhaps add a string output option?

---
+++ tests/c_misc.c
@@ -0,0 +1,70 @@
+// -*-c++-*-
+//
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// MASA - Manufactured Analytical Solutions Abstraction Library
+//
+// Copyright (C) 2010 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// $Author: nick $
+// $Id: c_euler1d.c 13404 2010-09-15 02:56:33Z nick $
+//
+// c_misc.c :program that tests masa helper functions
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#include <config.h>
+#include <masa.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  // list all currently initialized mms
+  cmasa_list_mms();
+
+  // initalize two different functions
+  cmasa_init("euler-test","euler_1d");
+  cmasa_init_param();
+  cmasa_init("euler-test2","euler_2d");
+  cmasa_init_param();
+
+  cmasa_list_mms();
+
+  // display parameters @ default values
+  cmasa_display_param();
+  
+  //change parameter
+  cmasa_set_param("u_0",2.3);
+
+  // display again
+  cmasa_display_param();
+
+  //switch to original function
+  cmasa_select_mms("euler-test");
+  cmasa_display_param();
+
+  //tests passed
+  return 0;
+
+}
+
