fd30a540d506c18d4ebee627e0672d67757b5d10
Nicholas Malaya <nicholas.malaya@gmail.com>
2010-09-21 01:08:41 +0000

    [masa]: euler 2d fortmasa now functioning...just 5 more to go. Yippie.

--- tests/f_euler2d.f90
+++ tests/f_euler2d.f90
@@ -163,22 +163,26 @@ program main
 	rho_an = masa_eval_2d_rho_an    (x,y)
 		  
         ! check against maple
-	ufield2 = eval_2d_u_source  (x,y,u_0,u_x,u_y,v_0,v_x,v_y,rho_0,rho_x,rho_y,p_0,p_x,p_y,a_px,a_py, &
-             a_rhox,a_rhoy,a_ux,a_uy,a_vx,a_vy,L)
+        ufield2 = eval_2d_u_source  (%val(x),%val(y),%val(u_0),%val(u_x),%val(u_y),%val(v_0),%val(v_x),%val(v_y), &
+             %val(rho_0),%val(rho_x),%val(rho_y),%val(p_0),%val(p_x),%val(p_y),%val(a_px),%val(a_py), &
+             %val(a_rhox),%val(a_rhoy),%val(a_ux),%val(a_uy),%val(a_vx),%val(a_vy),%val(L))
 
-	vfield2 = eval_2d_v_source  (x,y,u_0,u_x,u_y,v_0,v_x,v_y,rho_0,rho_x,rho_y,p_0,p_x,p_y,a_px,a_py, &
-             a_rhox,a_rhoy,a_ux,a_uy,a_vx,a_vy,L)
+	vfield2 = eval_2d_v_source  (%val(x),%val(y),%val(u_0),%val(u_x),%val(u_y),%val(v_0),%val(v_x),%val(v_y), &
+             %val(rho_0),%val(rho_x),%val(rho_y),%val(p_0),%val(p_x),%val(p_y),%val(a_px),%val(a_py), &
+             %val(a_rhox),%val(a_rhoy),%val(a_ux),%val(a_uy),%val(a_vx),%val(a_vy),%val(L))
 
-        rho2    = eval_2d_rho_source(x,y,u_0,u_x,u_y,v_0,v_x,v_y,rho_0,rho_x,rho_y,p_0,p_x,p_y,a_px,a_py, &
-             a_rhox,a_rhoy,a_ux,a_uy,a_vx,a_vy,L)
+        rho2    = eval_2d_rho_source(%val(x),%val(y),%val(u_0),%val(u_x),%val(u_y),%val(v_0),%val(v_x),%val(v_y), &
+             %val(rho_0),%val(rho_x),%val(rho_y),%val(p_0),%val(p_x),%val(p_y),%val(a_px),%val(a_py), &
+             %val(a_rhox),%val(a_rhoy),%val(a_ux),%val(a_uy),%val(a_vx),%val(a_vy),%val(L))
  
-        efield2 = eval_2d_e_source  (x,y,u_0,u_x,u_y,v_0,v_x,v_y,rho_0,rho_x,rho_y,p_0,p_x,p_y,a_px, &
-             a_py,a_rhox,a_rhoy,a_ux,a_uy,a_vx,a_vy,Gamma,mu,L)
+        efield2 = eval_2d_e_source  (%val(x),%val(y),%val(u_0),%val(u_x),%val(u_y),%val(v_0),%val(v_x),%val(v_y), &
+             %val(rho_0),%val(rho_x),%val(rho_y),%val(p_0),%val(p_x),%val(p_y),%val(a_px),%val(a_py), &
+             %val(a_rhox),%val(a_rhoy),%val(a_ux),%val(a_uy),%val(a_vx),%val(a_vy),%val(Gamma),%val(mu),%val(L))
 
-        u_an2   = eval_2d_u_an  (x,y,u_0,u_x,u_y,a_ux,a_uy,L)
-        v_an2   = eval_2d_v_an  (x,y,v_0,v_x,v_y,a_vx,a_vy,L)
-        rho_an2 = eval_2d_rho_an(x,y,rho_0,rho_x,rho_y,a_rhox,a_rhoy,L)
-        p_an2   = eval_2d_p_an  (x,y,p_0,p_x,p_y,a_px,a_py,L)
+        u_an2   = eval_2d_u_an  (%val(x),%val(y),%val(u_0),%val(u_x),%val(u_y),%val(a_ux),%val(a_uy),%val(L))
+        v_an2   = eval_2d_v_an  (%val(x),%val(y),%val(v_0),%val(v_x),%val(v_y),%val(a_vx),%val(a_vy),%val(L))
+        rho_an2 = eval_2d_rho_an(%val(x),%val(y),%val(rho_0),%val(rho_x),%val(rho_y),%val(a_rhox),%val(a_rhoy),%val(L))
+        p_an2   = eval_2d_p_an  (%val(x),%val(y),%val(p_0),%val(p_x),%val(p_y),%val(a_px),%val(a_py),%val(L))
 
         ! need to add strict / non-strict regressions
         ufield3 = abs(ufield-ufield2)
