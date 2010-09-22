862c411334b6c851a60cb3d194db6aae109f51cb
Karl W. Schulz <karl@koomie.com>
2010-09-22 22:37:23 +0000

    [masa]: adding nifty fortran license header support

--- tests/f_euler2d.f90
+++ tests/f_euler2d.f90
@@ -1,31 +1,31 @@
-! -----------------------------------------------------------------------bl-
-! --------------------------------------------------------------------------
-! 
-!  MASA - Manufactured Analytical Solutions Abstraction Library
-! 
-!  Copyright (C) 2010 The PECOS Development Team
-! 
-!  This library is free software; you can redistribute it and/or
-!  modify it under the terms of the Version 2.1 GNU Lesser General
-!  Public License as published by the Free Software Foundation.
-! 
-!  This library is distributed in the hope that it will be useful,
-!  but WITHOUT ANY WARRANTY; without even the implied warranty of
-!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-!  Lesser General Public License for more details.
-! 
-!  You should have received a copy of the GNU Lesser General Public
-!  License along with this library; if not, write to the Free Software
-!  Foundation, Inc. 51 Franklin Street, Fifth Floor,
-!  Boston, MA  02110-1301  USA
-! 
-! -----------------------------------------------------------------------el-
-!
-! f_euler2d.f90: Fortran Euler 2d test
-!
-! $Id: masa.f90 13217 2010-09-11 04:30:16Z nick $
-! --------------------------------------------------------------------------
-! --------------------------------------------------------------------------
+!!-----------------------------------------------------------------------bl-
+!!--------------------------------------------------------------------------
+!!
+!! MASA - Manufactured Analytical Solutions Abstraction Library
+!!
+!! Copyright (C) 2010 The PECOS Development Team
+!!
+!! This library is free software; you can redistribute it and/or
+!! modify it under the terms of the Version 2.1 GNU Lesser General
+!! Public License as published by the Free Software Foundation.
+!!
+!! This library is distributed in the hope that it will be useful,
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+!! Lesser General Public License for more details.
+!!
+!! You should have received a copy of the GNU Lesser General Public
+!! License along with this library; if not, write to the Free Software
+!! Foundation, Inc. 51 Franklin Street, Fifth Floor,
+!! Boston, MA  02110-1301  USA
+!!
+!!-----------------------------------------------------------------------el-
+!!
+!! f_euler2d.f90: Fortran Euler 2d test
+!!
+!! $Id: masa.f90 13217 2010-09-11 04:30:16Z nick $
+!! -------------------------------------------------------------------------
+!! -------------------------------------------------------------------------
 
 program main
   use euler_source_interface
