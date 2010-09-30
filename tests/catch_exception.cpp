7a4cf9937c3b966c8884572481981a2156c99c25
Nicholas Malaya <nicholas.malaya@gmail.com>
2010-09-30 23:28:58 +0000

    [masa]: forgot to add the actual catch exception routine! #1068

---
+++ tests/catch_exception.cpp
@@ -0,0 +1,83 @@
+// -*-c++-*-
+//
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// MASA - Manufactured Analytical Solutions Abstraction Library
+//
+// Copyright (C) 2010 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// $Author: nick $
+// $Id: misc.cpp 13822 2010-09-21 18:14:25Z nick $
+//
+// catch_exception.cpp : program that tests fatal masa failures
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#include <config.h>
+#include <masa.h>
+#include <iostream>
+#include <stdio.h>
+
+using namespace MASA;
+using namespace std;
+
+int main()
+{
+
+  // only test with masa exception handling turned on
+#ifdef MASA_EXCEPTIONS
+  
+  // reroute stdout for regressions: TODO remove when logger mechanism
+  // is used inside masa; these tests currently just verify functions
+  // run successfully.
+  freopen("/dev/null","w",stdout);
+
+  // fatal error check:  
+  try
+    {
+      masa_init("it is pitch black","grue"); // grue is not a valid masa mms, nor should it be
+    }
+  catch(int err) // return one on fatal error
+    {
+      if(err != 1)
+	{
+	  cout << "regression test failed: masa_init fatal error condition not triggered!\n";
+	  return 1; // fail
+	}      
+    }
+ 
+  try
+    {
+      masa_select_mms("cthulhu"); // cthulhu is not a valid masa solution, nor has it been intialized
+    }
+  catch(int err)
+    {
+      if(err != 1)
+	{
+	  cout << "regression test failed: masa_select_mms fatal error condition not triggered!\n";
+	  return 1; // fail
+	}      
+    }
+
+#endif
+
+  return 0; // success
+
+}
