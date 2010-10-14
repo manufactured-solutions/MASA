cc7a8b89904b77f451ba240a654f2a2123e0fe84
Nicholas Malaya <nicholas.malaya@gmail.com>
2010-10-14 16:52:16 +0000

    [masa]: adding simple driver routine for displaying available solutions in masa. closing issue #1083

---
+++ examples/display_solutions.cpp
@@ -0,0 +1,46 @@
+// -*-c++-*-
+//
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// MASA - Manufactured Analytical Solutions Abstraction Library
+//
+// Copyright (C) 2010 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// $Author: nick $
+// $Id: verify.cpp 12695 2010-08-26 03:47:26Z nick $
+//
+//
+// verify.cpp: test routine to check the conjugate gradient solver
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+//
+#include <iostream>
+#include <math.h>
+#include <masa.h>
+
+using namespace MASA;
+using namespace std;
+
+int main()
+{
+  // show available solutions
+  masa_printid();
+
+}
