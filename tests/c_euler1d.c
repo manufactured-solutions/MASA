5576d34b37e7e0e598c2d4345fb12bc16474e654
Nicholas Malaya <nicholas.malaya@gmail.com>
2011-02-02 17:14:55 +0000

    [masa-tests]: seriously, this time all the regression tests should work!

--- tests/c_euler1d.c
+++ tests/c_euler1d.c
@@ -79,14 +79,13 @@ double SourceQ_rho (
 {
   const double pi = acos(-1);  
   double Q_rho;
-  double Q_rho;
   double RHO;
   double U;
 
   RHO = rho_0 + rho_x * sin(a_rhox * pi * x / L);
   U = u_0 + u_x * sin(a_ux * pi * x / L);
 
-  Q_rho = cos(a_ux * PI * x / L) * RHO * a_ux * PI * u_x / L + cos(a_rhox * PI * x / L) * U * a_rhox * PI * rho_x / L;
+  Q_rho = cos(a_ux * pi * x / L) * RHO * a_ux * pi * u_x / L + cos(a_rhox * pi * x / L) * U * a_rhox * pi * rho_x / L;
 
   return(Q_rho);
 }
@@ -106,14 +105,13 @@ double SourceQ_u (
 {
   const double pi = acos(-1); 
   double Q_u;
-  double Q_u;
   double RHO;
   double U;
 
   RHO = rho_0 + rho_x * sin(a_rhox * pi * x / L);
   U = u_0 + u_x * sin(a_ux * pi * x / L);
 
-  Q_u = 0.2e1 * cos(a_ux * PI * x / L) * RHO * U * a_ux * PI * u_x / L + cos(a_rhox * PI * x / L) * U * U * a_rhox * PI * rho_x / L - sin(a_px * PI * x / L) * a_px * PI * p_x / L;
+  Q_u = 0.2e1 * cos(a_ux * pi * x / L) * RHO * U * a_ux * pi * u_x / L + cos(a_rhox * pi * x / L) * U * U * a_rhox * pi * rho_x / L - sin(a_px * pi * x / L) * a_px * pi * p_x / L;
   
   return(Q_u);
 }
@@ -133,8 +131,7 @@ double SourceQ_e (
   double mu,
   double L)
 {
-  //const double pi = acos(-1);  
-  const double pi = M_PI;
+  const double pi = acos(-1);  
   double Q_e;
   double RHO;
   double U;
@@ -144,7 +141,7 @@ double SourceQ_e (
   P = p_0 + p_x * cos(a_px * pi * x / L);
   U = u_0 + u_x * sin(a_ux * pi * x / L);
 
-  Q_e = cos(a_rhox * PI * x / L) * pow(U, 0.3e1) * a_rhox * PI * rho_x / L / 0.2e1 + cos(a_ux * PI * x / L) * P * a_ux * PI * u_x * Gamma / L / (Gamma - 0.1e1) + 0.3e1 / 0.2e1 * cos(a_ux * PI * x / L) * RHO * U * U * a_ux * PI * u_x / L - sin(a_px * PI * x / L) * U * a_px * PI * p_x * Gamma / L / (Gamma - 0.1e1);
+  Q_e = cos(a_rhox * pi * x / L) * pow(U, 0.3e1) * a_rhox * pi * rho_x / L / 0.2e1 + cos(a_ux * pi * x / L) * P * a_ux * pi * u_x * Gamma / L / (Gamma - 0.1e1) + 0.3e1 / 0.2e1 * cos(a_ux * pi * x / L) * RHO * U * U * a_ux * pi * u_x / L - sin(a_px * pi * x / L) * U * a_px * pi * p_x * Gamma / L / (Gamma - 0.1e1);
   return(Q_e);
 }
 
