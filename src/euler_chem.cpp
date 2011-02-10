b349b503a96b45da0ebfd3659068a2023d5a69a5
Nicholas Malaya <nicholas.malaya@gmail.com>
2011-02-10 20:26:31 +0000

    [masa-tests]: populating tests with actual source terms

--- src/euler_chem.cpp
+++ src/euler_chem.cpp
@@ -186,13 +186,14 @@ Scalar MASA::euler_chem_1d<Scalar>::eval_q_rho_e(Scalar x)
   Scalar alpha;
   Scalar E_vib_N2;
 
-  alpha = exp(theta_v_N2 / T);
-  E_vib_N2 = R_N2 * theta_v_N2 / (alpha - 0.1e1);
   RHO_N = rho_N_0 + rho_N_x * sin(a_rho_N_x * PI * x / L);
   RHO_N2 = rho_N2_0 + rho_N2_x * cos(a_rho_N2_x * PI * x / L);
   RHO = RHO_N + RHO_N2;
   U = u_0 + u_x * sin(a_ux * PI * x / L);
   T = T_0 + T_x * cos(a_Tx * PI * x / L);
+  alpha = exp(theta_v_N2 / T);
+  E_vib_N2 = R_N2 * theta_v_N2 / (alpha - 0.1e1);
+
 
   Q_e = -a_Tx * PI * T_x * alpha * theta_v_N2 * E_vib_N2 * RHO_N2 * U * sin(a_Tx * PI * x / L) / L / (alpha - 0.1e1) * pow(T, -0.2e1) - 0.5e1 / 0.2e1 * a_Tx * PI * T_x * R_N * RHO_N * U * sin(a_Tx * PI * x / L) / L - 0.7e1 / 0.4e1 * a_Tx * PI * T_x * R_N * RHO_N2 * U * sin(a_Tx * PI * x / L) / L + 0.5e1 / 0.2e1 * a_ux * PI * u_x * R_N * RHO_N * T * cos(a_ux * PI * x / L) / L + 0.7e1 / 0.4e1 * a_ux * PI * u_x * R_N * RHO_N2 * T * cos(a_ux * PI * x / L) / L + 0.3e1 / 0.2e1 * a_ux * PI * u_x * RHO * U * U * cos(a_ux * PI * x / L) / L - a_rho_N2_x * PI * rho_N2_x * E_vib_N2 * U * sin(a_rho_N2_x * PI * x / L) / L + a_ux * PI * u_x * E_vib_N2 * RHO_N2 * cos(a_ux * PI * x / L) / L + (h0_N * RHO_N + h0_N2 * RHO_N2) * a_ux * PI * u_x * cos(a_ux * PI * x / L) / L - (-0.10e2 * a_rho_N_x * rho_N_x * cos(a_rho_N_x * PI * x / L) + 0.7e1 * a_rho_N2_x * rho_N2_x * sin(a_rho_N2_x * PI * x / L)) * PI * R_N * U * T / L / 0.4e1 - (-a_rho_N_x * rho_N_x * cos(a_rho_N_x * PI * x / L) + a_rho_N2_x * rho_N2_x * sin(a_rho_N2_x * PI * x / L)) * PI * pow(U, 0.3e1) / L / 0.2e1 - (-a_rho_N_x * rho_N_x * h0_N * cos(a_rho_N_x * PI * x / L) + a_rho_N2_x * rho_N2_x * h0_N2 * sin(a_rho_N2_x * PI * x / L)) * PI * U / L;
 
--- tests/euler_chem_1d.cpp
+++ tests/euler_chem_1d.cpp
@@ -51,6 +51,181 @@ Scalar nancheck(Scalar x)
   return 1;
 }
 
+
+template <typename Scalar>
+Scalar SourceQ_rho_u(Scalar x, Scalar R_N)
+{
+  Scalar Q_u;
+  Scalar RHO;
+  Scalar RHO_N;
+  Scalar RHO_N2;
+  Scalar U;
+  Scalar T;
+
+  RHO_N = rho_N_0 + rho_N_x * sin(a_rho_N_x * PI * x / L);
+  RHO_N2 = rho_N2_0 + rho_N2_x * cos(a_rho_N2_x * PI * x / L);
+  RHO = RHO_N + RHO_N2;
+  U = u_0 + u_x * sin(a_ux * PI * x / L);
+  T = T_0 + T_x * cos(a_Tx * PI * x / L);
+
+  Q_u = a_rho_N_x * PI * rho_N_x * U * U * cos(a_rho_N_x * PI * x / L) / L - a_rho_N2_x * PI * rho_N2_x * U * U * sin(a_rho_N2_x * PI * x / L) / L - a_Tx * PI * T_x * R_N * RHO_N * sin(a_Tx * PI * x / L) / L - a_Tx * PI * T_x * R_N * RHO_N2 * sin(a_Tx * PI * x / L) / L / 0.2e1 + 0.2e1 * a_ux * PI * u_x * RHO * U * cos(a_ux * PI * x / L) / L - (-0.2e1 * a_rho_N_x * rho_N_x * cos(a_rho_N_x * PI * x / L) + a_rho_N2_x * rho_N2_x * sin(a_rho_N2_x * PI * x / L)) * PI * R_N * T / L / 0.2e1;
+  return(Q_u);
+}
+
+template <typename Scalar>
+Scalar SourceQ_rho_e(Scalar x,
+		    Scalar R_N,
+		    Scalar R_N2,
+		    Scalar h0_N,
+		    Scalar h0_N2,
+		    Scalar theta_v_N2)
+{
+  Scalar Q_e;
+  Scalar RHO;
+  Scalar RHO_N;
+  Scalar RHO_N2;
+  Scalar U;
+  Scalar T;
+  Scalar alpha;
+  Scalar E_vib_N2;
+
+  RHO_N = rho_N_0 + rho_N_x * sin(a_rho_N_x * PI * x / L);
+  RHO_N2 = rho_N2_0 + rho_N2_x * cos(a_rho_N2_x * PI * x / L);
+  RHO = RHO_N + RHO_N2;
+  U = u_0 + u_x * sin(a_ux * PI * x / L);
+  T = T_0 + T_x * cos(a_Tx * PI * x / L);
+  alpha = exp(theta_v_N2 / T);
+  E_vib_N2 = R_N2 * theta_v_N2 / (alpha - 0.1e1);
+
+  Q_e = -a_Tx * PI * T_x * alpha * theta_v_N2 * E_vib_N2 * RHO_N2 * U * sin(a_Tx * PI * x / L) / L / (alpha - 0.1e1) * pow(T, -0.2e1) - 0.5e1 / 0.2e1 * a_Tx * PI * T_x * R_N * RHO_N * U * sin(a_Tx * PI * x / L) / L - 0.7e1 / 0.4e1 * a_Tx * PI * T_x * R_N * RHO_N2 * U * sin(a_Tx * PI * x / L) / L + 0.5e1 / 0.2e1 * a_ux * PI * u_x * R_N * RHO_N * T * cos(a_ux * PI * x / L) / L + 0.7e1 / 0.4e1 * a_ux * PI * u_x * R_N * RHO_N2 * T * cos(a_ux * PI * x / L) / L + 0.3e1 / 0.2e1 * a_ux * PI * u_x * RHO * U * U * cos(a_ux * PI * x / L) / L - a_rho_N2_x * PI * rho_N2_x * E_vib_N2 * U * sin(a_rho_N2_x * PI * x / L) / L + a_ux * PI * u_x * E_vib_N2 * RHO_N2 * cos(a_ux * PI * x / L) / L + (h0_N * RHO_N + h0_N2 * RHO_N2) * a_ux * PI * u_x * cos(a_ux * PI * x / L) / L - (-0.10e2 * a_rho_N_x * rho_N_x * cos(a_rho_N_x * PI * x / L) + 0.7e1 * a_rho_N2_x * rho_N2_x * sin(a_rho_N2_x * PI * x / L)) * PI * R_N * U * T / L / 0.4e1 - (-a_rho_N_x * rho_N_x * cos(a_rho_N_x * PI * x / L) + a_rho_N2_x * rho_N2_x * sin(a_rho_N2_x * PI * x / L)) * PI * pow(U, 0.3e1) / L / 0.2e1 - (-a_rho_N_x * rho_N_x * h0_N * cos(a_rho_N_x * PI * x / L) + a_rho_N2_x * rho_N2_x * h0_N2 * sin(a_rho_N2_x * PI * x / L)) * PI * U / L;
+
+  return(Q_e);
+}
+
+template <typename Scalar>
+Scalar SourceQ_rho_N(Scalar x,
+		    Scalar M_N,
+		    Scalar h0_N,
+		    Scalar h0_N2,
+		    Scalar Cf1_N,
+		    Scalar Cf1_N2,
+		    Scalar etaf1_N,
+		    Scalar etaf1_N2,
+		    Scalar Ea_N,
+		    Scalar Ea_N2,
+		    Scalar Function_to_Calculate_K)
+{
+  Scalar Q_rho_N;
+  Scalar RHO_N;
+  Scalar RHO_N2;
+  Scalar U;
+  Scalar T;
+  Scalar kf1_N;
+  Scalar kf1_N2;
+  Scalar K_eq;
+
+  K_eq = Function_to_Calculate_K;
+  RHO_N = rho_N_0 + rho_N_x * sin(a_rho_N_x * PI * x / L);
+  RHO_N2 = rho_N2_0 + rho_N2_x * cos(a_rho_N2_x * PI * x / L);
+  U = u_0 + u_x * sin(a_ux * PI * x / L);
+  T = T_0 + T_x * cos(a_Tx * PI * x / L);
+  kf1_N = Cf1_N * pow(T, etaf1_N) * exp(-Ea_N / R / T);
+  kf1_N2 = Cf1_N2 * pow(T, etaf1_N2) * exp(-Ea_N2 / R / T);
+
+  Q_rho_N = a_rho_N_x * PI * rho_N_x * U * cos(a_rho_N_x * PI * x / L) / L + a_ux * PI * u_x * RHO_N * cos(a_ux * PI * x / L) / L + (0.2e1 * kf1_N * RHO_N + kf1_N2 * RHO_N2) * RHO_N * RHO_N * pow(M_N, -0.2e1) / K_eq - (0.2e1 * kf1_N * RHO_N + kf1_N2 * RHO_N2) * RHO_N2 / M_N / 0.2e1;
+
+  return(Q_rho_N);
+}
+
+template <typename Scalar>
+Scalar SourceQ_rho_N2(Scalar x,
+		     Scalar M_N,
+		     Scalar h0_N,
+		     Scalar h0_N2,
+		     Scalar K,
+		     Scalar Cf1_N,
+		     Scalar Cf1_N2,
+		     Scalar etaf1_N,
+		     Scalar etaf1_N2,
+		     Scalar Ea_N,
+		     Scalar Ea_N2,
+		     Scalar Function_to_Calculate_K)
+{
+  
+  Scalar Q_rho_N2;
+  Scalar RHO_N;
+  Scalar RHO_N2;
+  Scalar U;
+  Scalar T;
+  Scalar kf1_N;
+  Scalar kf1_N2;
+  Scalar K_eq;
+
+  K_eq = Function_to_Calculate_K;
+  RHO_N = rho_N_0 + rho_N_x * sin(a_rho_N_x * PI * x / L);
+  RHO_N2 = rho_N2_0 + rho_N2_x * cos(a_rho_N2_x * PI * x / L);
+  U = u_0 + u_x * sin(a_ux * PI * x / L);
+  T = T_0 + T_x * cos(a_Tx * PI * x / L);
+  kf1_N = Cf1_N * pow(T, etaf1_N) * exp(-Ea_N / R / T);
+  kf1_N2 = Cf1_N2 * pow(T, etaf1_N2) * exp(-Ea_N2 / R / T);
+
+  Q_rho_N2 = -a_rho_N2_x * PI * rho_N2_x * U * sin(a_rho_N2_x * PI * x / L) / L + a_ux * PI * u_x * RHO_N2 * cos(a_ux * PI * x / L) / L - (0.2e1 * kf1_N * RHO_N + kf1_N2 * RHO_N2) * RHO_N * RHO_N * pow(M_N, -0.2e1) / K_eq + (0.2e1 * kf1_N * RHO_N + kf1_N2 * RHO_N2) * RHO_N2 / M_N / 0.2e1;
+  return(Q_rho_N2);
+}
+
+// ----------------------------------------
+//   Analytical Terms
+// ----------------------------------------
+
+template <typename Scalar>
+Scalar anQ_t(Scalar x)
+{
+  Scalar T_an = T_0 + T_x * cos(a_Tx * pi * x / L);
+  return T_an;
+}
+
+template <typename Scalar>
+Scalar anQ_u(Scalar x)
+{
+  Scalar u_an = u_0 + u_x * sin(a_ux * pi * x / L);
+  return u_an;
+}
+
+template <typename Scalar>
+Scalar anQ_rho(Scalar x)
+{
+  Scalar rho_an = rho_N_0 + rho_N_x * sin(a_rho_N_x * pi * x / L) + rho_N2_0 + rho_N2_x * cos(a_rho_N2_x * pi * x / L);
+  return rho_an;
+}
+
+template <typename Scalar>
+Scalar anQ_rho_N(Scalar x)
+{
+  Scalar rho_an_N = rho_N_0 + rho_N_x * sin(a_rho_N_x * pi * x / L);
+  return rho_an_N;
+}
+
+template <typename Scalar>
+Scalar anQ_rho_N2(Scalar x)
+{
+  Scalar rho_an_N2 = rho_N2_0 + rho_N2_x * cos(a_rho_N2_x * pi * x / L);
+  return rho_an_N2;
+}
+
+// ----------------------------------------
+//   Regresssion
+// ----------------------------------------
+
+template<typename Scalar>
+Scalar temp_function(Scalar T)
+{
+  // hackish functional here
+  // That's an eyeballed fit (focusing on the 5000K-6000K range) 
+  // for the equilibrium constant for N2->N+N dissociation
+  Scalar K = exp(4+(T-6000)/500);
+  return K;
+}
+
 template<typename Scalar>
 int run_regression()
 {
