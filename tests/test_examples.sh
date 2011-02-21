b634e513fe620ecdccb40f02712a4a5d02daeccd
Nicholas Malaya <nicholas.malaya@gmail.com>
2011-02-21 16:46:16 +0000

    [masa]: adding example directory test script, need to populate with all masa examples! #1409

---
+++ tests/test_examples.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# This script runs each example in the examples directory
+#
+# It is designed to check that the masa api 
+# has not changed
+#
+# Return 0 for success
+#        1 upon failure
+
+err=0
+
+echo " "
+echo "-------------------------------------------------------"
+echo "Initializing MASA Examples Tests"
+echo "-------------------------------------------------------"
+
+exit $err
+
+#
+# written by: Nicholas Malaya
+#             2/21/2011
