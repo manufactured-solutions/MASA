c4dca985339f096d3335de08b33b34f5ecd3c674
Nicholas Malaya <nicholas.malaya@gmail.com>
2011-02-22 00:47:09 +0000

    [masa]: just one last tweak and all should be good to go

--- examples/f_euler.f90
+++ examples/f_euler.f90
@@ -1,15 +1,41 @@
+!! -*-f90-*-
+!!-----------------------------------------------------------------------bl-
+!!--------------------------------------------------------------------------
+!!
+!! MASA - Manufactured Analytical Solutions Abstraction Library
+!!
+!! Copyright (C) 2010 The PECOS Development Team
+!!
+!! This library is free software; you can redistribute it and/or
+!! modify it under the terms of the Version 2.1 GNU Lesser General
+!! Public License as published by the Free Software Foundation.
+!!
+!! This library is distributed in the hope that it will be useful,
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+!! Lesser General Public License for more details.
+!!
+!! You should have received a copy of the GNU Lesser General Public
+!! License along with this library; if not, write to the Free Software
+!! Foundation, Inc. 51 Franklin Street, Fifth Floor,
+!! Boston, MA  02110-1301  USA
+!!
+!!-----------------------------------------------------------------------el-
+!!
+!! f_cns.f90: Fortran Euler Example
+!!
+!! $Id: 
+!! -------------------------------------------------------------------------
+!! -------------------------------------------------------------------------
+
 program main
   use masa
   use iso_c_binding
-
   implicit none
 
   ! declarations
   real(8) :: value
   real(8) :: out 
-
-  ! problem size
-
 
   ! initialize the problem
   call masa_init('mytest','euler_1d')
@@ -45,5 +71,5 @@ program main
   out = masa_eval_1d_source_u(value)
   write(6,*) "value is: ", out 
 
-  stop
+  call exit(0)
 end program main
