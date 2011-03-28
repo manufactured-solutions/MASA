da812a8e2ca82280512d7aed008a3f247811ae8b
Nicholas Malaya <nicholas.malaya@gmail.com>
2011-03-28 20:12:54 +0000

    [masa]: adding array regression test

---
+++ tests/c_array.c
@@ -0,0 +1,52 @@
+// -*-c++-*-
+//
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// MASA - Manufactured Analytical Solutions Abstraction Library
+//
+// Copyright (C) 2010 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// $Author: nick $
+// $Id: uninit.cpp 18117 2011-02-28 05:10:35Z nick $
+//
+// c_array.c : program that tests array capability in masa
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#include <config.h>
+#include <masa.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int err=0;
+  
+  // initialize the problem
+  err += masa_init("radiation","radiation_integrated_intensity");
+
+  // reroute stdout for regressions: TODO remove when logger mechanism
+  // is used inside masa; these tests currently just verify functions
+  // run successfully.
+  //freopen("/dev/null","w",stdout);
+
+  return 0;
+
+}
