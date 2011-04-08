13c019718ebf80fff9771dbacef34a45ef89b55b
Nicholas Malaya <nicholas.malaya@gmail.com>
2011-04-08 14:34:10 +0000

    [masa]: adding masa_get_array in fortran

--- tests/f_array.F90
+++ tests/f_array.F90
@@ -31,43 +31,8 @@ program main
   use masa
   implicit none
 
-  real(8) :: value = 0
-
-  ! initialize the problem
-  call masa_init('mytest','euler_1d')
-  ! initialize the default parameters
-  call masa_init_param()
-
-  call masa_init('2nd test','euler_3d')
-  call masa_init_param()
-  
-  ! test get_param function
-  value = masa_get_param("L")
-
-  if(value .ne. 3.02d0) then
-     write(6,*) "FortMASA REGRESSION FAILURE: get param failure"
-     write(6,*) "Exiting"
-     call exit(1)
-  endif 
-
-  ! test set_param function
-  call masa_set_param("L",3.1415d0)
-  value = masa_get_param("L")
-
-  if(value .ne. 3.1415d0) then
-     write(6,*) "FortMASA REGRESSION FAILURE: set param failure"
-     write(6,*) "Exiting"
-     call exit(1)
-  endif 
-
-  ! test select_mms
-  call masa_select_mms("mytest")
-  value = masa_get_param("L") ! this should no longer be 3.14
-
-  if(value .eq. 3.1415) then
-     write(6,*) "FortMASA REGRESSION FAILURE: masa_switch failure"
-     write(6,*) "Exiting"
-     call exit(1)
-  endif 
+  ! test display_array
+  call masa_init('3rd test','radiation_integrated_intensity')
+  call masa_display_array()
 
 end program
