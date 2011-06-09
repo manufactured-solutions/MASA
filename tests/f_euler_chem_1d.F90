ff544daaaa966780048e34ab943c3e1002353940
Karl W. Schulz <karl@koomie.com>
2011-06-09 19:11:22 +0000

    [masa]: remove some semi's in fortran

--- tests/f_euler_chem_1d.F90
+++ tests/f_euler_chem_1d.F90
@@ -52,7 +52,7 @@ program main
   use masa
   implicit none
 
-  real(8) :: MASA_DEFAULT = -12345.67d0;
+  real(8) :: MASA_DEFAULT = -12345.67d0
 
   real(8) :: u_0
   real(8) :: u_x
@@ -92,17 +92,17 @@ program main
   real(8) :: dx
 
   ! solutions
-  real(8) :: ufield,ufield2,ufield3;
-  real(8) :: efield,efield2,efield3;
-  real(8) :: N,N2,N3;
-  real(8) :: Ntwo,Ntwo2,Ntwo3;
-  real(8) :: gradx,grady,gradz,gradp,gradrho;
+  real(8) :: ufield,ufield2,ufield3
+  real(8) :: efield,efield2,efield3
+  real(8) :: N,N2,N3
+  real(8) :: Ntwo,Ntwo2,Ntwo3
+  real(8) :: gradx,grady,gradz,gradp,gradrho
 
-  real(8) :: exact_t,exact_t2,exact_t3;
-  real(8) :: exact_u,exact_u2,exact_u3;
-  real(8) :: exact_rho,exact_rho2,exact_rho3;
-  real(8) :: exact_N,exact_N2,exact_N3;
-  real(8) :: exact_Ntwo,exact_Ntwo2,exact_Ntwo3;
+  real(8) :: exact_t,exact_t2,exact_t3
+  real(8) :: exact_u,exact_u2,exact_u3
+  real(8) :: exact_rho,exact_rho2,exact_rho3
+  real(8) :: exact_N,exact_N2,exact_N3
+  real(8) :: exact_Ntwo,exact_Ntwo2,exact_Ntwo3
 
   ! iterators
   integer :: i
@@ -111,50 +111,54 @@ program main
   lx = 10
 
   ! initialize the problem
+
   dx = real(lx)/real(nx)
 
   call masa_init("mytest","euler_chem_1d")
 
   ! initialize the default parameters
+
   call masa_init_param()
 
   ! get defaults for comparison to source terms
-  u_0  = masa_get_param("u_0");
-  u_x  = masa_get_param("u_x");
-  a_ux = masa_get_param("a_ux");
-  L    = masa_get_param("L");
-  R    = masa_get_param("R");
 
-  Cf1_N   = masa_get_param("Cf1_N");
-  Cf1_N2  = masa_get_param("Cf1_N2");
-  etaf1_N  = masa_get_param("etaf1_N");
-  etaf1_N2 = masa_get_param("etaf1_N2");
+  u_0        = masa_get_param("u_0")
+  u_x        = masa_get_param("u_x")
+  a_ux       = masa_get_param("a_ux")
+  L          = masa_get_param("L")
+  R          = masa_get_param("R")
+             
+  Cf1_N      = masa_get_param("Cf1_N")
+  Cf1_N2     = masa_get_param("Cf1_N2")
+  etaf1_N    = masa_get_param("etaf1_N")
+  etaf1_N2   = masa_get_param("etaf1_N2")
+             
+  Ea_N       = masa_get_param("Ea_N")
+  Ea_N2      = masa_get_param("Ea_N2")
+             
+  R_N        = masa_get_param("R_N")
+  R_N2       = masa_get_param("R_N2")
 
-  Ea_N  = masa_get_param("Ea_N");
-  Ea_N2 = masa_get_param("Ea_N2");
+  theta_v_N2 = masa_get_param("theta_v_N2")
+  M_N        = masa_get_param("M_N")
+  h0_N       = masa_get_param("h0_N")
+  h0_N2      = masa_get_param("h0_N2")
 
-  R_N   = masa_get_param("R_N");
-  R_N2  = masa_get_param("R_N2");
+  rho_N_0    = masa_get_param("rho_N_0")
+  rho_N_x    = masa_get_param("rho_N_x")
+  a_rho_N_x  = masa_get_param("a_rho_N_x")
 
-  theta_v_N2 = masa_get_param("theta_v_N2");
-  M_N   = masa_get_param("M_N");
-  h0_N  = masa_get_param("h0_N");
-  h0_N2 = masa_get_param("h0_N2");
-
-  rho_N_0   = masa_get_param("rho_N_0");
-  rho_N_x   = masa_get_param("rho_N_x");
-  a_rho_N_x = masa_get_param("a_rho_N_x");
-
-  rho_N2_0   = masa_get_param("rho_N2_0");  
-  rho_N2_x   = masa_get_param("rho_N2_x");
-  a_rho_N2_x = masa_get_param("a_rho_N2_x");
+  rho_N2_0   = masa_get_param("rho_N2_0")  
+  rho_N2_x   = masa_get_param("rho_N2_x")
+  a_rho_N2_x = masa_get_param("a_rho_N2_x")
   
-  T_0  = masa_get_param("T_0");
-  T_x  = masa_get_param("T_x");
-  a_Tx = masa_get_param("a_Tx");
+  T_0        = masa_get_param("T_0")
+  T_x        = masa_get_param("T_x")
+  a_Tx       = masa_get_param("a_Tx")
 
   ! call the sanity check routine 
   ! (tests that all variables have been initialized)
+
   call masa_sanity_check()
 
   ! evaluate source terms (2D)
@@ -163,21 +167,24 @@ program main
      x = i * dx
      
      ! evalulate source terms
-     ufield = masa_eval_1d_source_rho_u (x);
-     efield = masa_eval_1d_source_rho_e (x);
-     N      = masa_eval_1d_source_rho_N (x,k_func);
-     Ntwo   = masa_eval_1d_source_rho_N2(x,k_func);
+
+     ufield     = masa_eval_1d_source_rho_u (x)
+     efield     = masa_eval_1d_source_rho_e (x)
+     N          = masa_eval_1d_source_rho_N (x,k_func)
+     Ntwo       = masa_eval_1d_source_rho_N2(x,k_func)
 
      ! evaluate analytical solution terms
-     exact_t    = masa_eval_1d_exact_t     (x);
-     exact_u    = masa_eval_1d_exact_u     (x);
-     exact_rho  = masa_eval_1d_exact_rho   (x);
-     exact_N    = masa_eval_1d_exact_rho_N (x);
-     exact_Ntwo = masa_eval_1d_exact_rho_N2(x);
+
+     exact_t    = masa_eval_1d_exact_t     (x)
+     exact_u    = masa_eval_1d_exact_u     (x)
+     exact_rho  = masa_eval_1d_exact_rho   (x)
+     exact_N    = masa_eval_1d_exact_rho_N (x)
+     exact_Ntwo = masa_eval_1d_exact_rho_N2(x)
      
   enddo
 
   ! steady as she goes...
+
   call exit(0)
 
 end program main
