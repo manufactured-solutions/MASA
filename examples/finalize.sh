528e2ce27529f37f0ec359a74d151417091cb1ec
Nicholas Malaya <nicholas.malaya@gmail.com>
2011-06-09 22:12:37 +0000

    [masa]: for #1830, adding shell scripts and prepping makefile.am

---
+++ examples/finalize.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo ""
+echo "-------------------------------------------------------"
+echo "Finalizing MASA Tests, have a well verified day"
+echo "-------------------------------------------------------"
+echo ""
