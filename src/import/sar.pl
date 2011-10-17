d0f558cf52d48cc67b7a728f5c343bfc2504fb36
Nicholas Malaya <nicholas.malaya@gmail.com>
2011-10-17 18:55:42 +0000

    [masa]: adding lots of dialogue for the user to know what is going on, #2076

--- src/import/sar.pl
+++ src/import/sar.pl
@@ -2,10 +2,39 @@
 # open file, find line, replace with special text
 use warnings;
 
-$soln     = 'blargles_friend';
+# start prompt
+print "\n";
+print " * ------------------------------------------------------------ *\n";
+print " * Welcome to the MASA solution import system.                  *\n";
+print " * This will import a manufactured solution you have generated. *\n";
+print " * ------------------------------------------------------------ *\n\n";
+print " Warning!! This functionality is experimental.\n"; 
+print " It is recommended you create a backup of your MASA local library before proceeding.\n";
+print " Continue? (y/n)\n";
+$line = <STDIN>;
+
+# error handling
+if($line =~ "y")
+{
+    print "Continuing...\n\n"
+}
+else
+{
+    print "Ending\n";
+    exit 0;
+}
+# get solution class name
+print " Please input the name of your new solution class:\n";
+$line = <STDIN>;
+chomp($line);
+print "\n";
+$soln     = $line;
 $new_masa = "  anim.push_back(new $soln<Scalar>());\n\n";
 
-# open file
+print "Instantiating $soln in masa_core.cpp...\n";
+print $new_masa;
+
+# open file(s)
 $old = "../masa_core.cpp";
 $new = "tmp";
 $bak = "masa_core.bak";
@@ -36,11 +65,22 @@ while($line = <INFILE>)
     }
 }
 
+print "Done with masa_core.cpp...\n";
+print "Cleaning up...\n\n";
+
 close  INFILE or die $!;
 close OUTFILE or die $!;
 
-rename($old, $bak);
-rename($new, $old);
+#rename($old, $bak);
+#rename($new, $old);
+
+print "Creating class in masa_internal.h...\n";
+
+print "Editing Makefile.am...\n";
+
+
+exit 0;
+
 
 # nick
 # 10/11/11
