640e80e5ea5f2d64ab95b541bde3d0733ac0de83
Nicholas Malaya <nicholas.malaya@gmail.com>
2012-05-31 14:16:25 +0000

    [masa]: updating ad example so it actually runs

--- src/import/examples/automatic_differentiation_example/source_terms.cpp
+++ src/import/examples/automatic_differentiation_example/source_terms.cpp
@@ -2,7 +2,9 @@
 // public static method, that can be called from eval_q_t
 double eval_q_u(double x, double y) const
 {
-  typedef typename RawType<ADScalar>::value_type Scalar;
+  typedef DualNumber<Scalar, NumberArray<NDIM, Scalar> > FirstDerivType;
+  typedef DualNumber<FirstDerivType, NumberArray<NDIM, FirstDerivType> > SecondDerivType;
+  typedef SecondDerivType ADScalar;
 
   // Treat velocity as a vector
   NumberArray<NDIM, ADScalar> U;
@@ -25,7 +27,7 @@ double eval_q_u(double x, double y) const
 
   // The identity tensor I
   NumberArray<NDIM, NumberArray<NDIM, Scalar> > Identity = 
-    NumberArrayIdentity<NDIM, Scalar>::identity();
+    NumberArray<NDIM, Scalar>::identity();
 
   // The shear stress tensor
   NumberArray<NDIM, NumberArray<NDIM, ADScalar> > Tau = mu * (GradU + transpose(GradU) - 2./3.*divergence(U)*Identity);
@@ -34,18 +36,19 @@ double eval_q_u(double x, double y) const
   NumberArray<NDIM, ADScalar> q = -k * T.derivatives();
 
   // Euler equation residuals
-  Scalar Q_rho = raw_value(divergence(RHO*U));
+  // Scalar Q_rho = raw_value(divergence(RHO*U));
   NumberArray<NDIM, Scalar> Q_rho_u = 
     raw_value(divergence(RHO*U.outerproduct(U) - Tau) + P.derivatives());
 
-  return U[0];
-
+  return Q_rho_u[0];
 }
 
 // public, static method
 double eval_q_v(double x, double y) const
 {
-  typedef typename RawType<ADScalar>::value_type Scalar;
+  typedef DualNumber<Scalar, NumberArray<NDIM, Scalar> > FirstDerivType;
+  typedef DualNumber<FirstDerivType, NumberArray<NDIM, FirstDerivType> > SecondDerivType;
+  typedef SecondDerivType ADScalar;
 
   // Treat velocity as a vector
   NumberArray<NDIM, ADScalar> U;
@@ -68,7 +71,7 @@ double eval_q_v(double x, double y) const
 
   // The identity tensor I
   NumberArray<NDIM, NumberArray<NDIM, Scalar> > Identity = 
-    NumberArrayIdentity<NDIM, Scalar>::identity();
+    NumberArray<NDIM, Scalar>::identity();
 
   // The shear stress tensor
   NumberArray<NDIM, NumberArray<NDIM, ADScalar> > Tau = mu * (GradU + transpose(GradU) - 2./3.*divergence(U)*Identity);
@@ -77,18 +80,20 @@ double eval_q_v(double x, double y) const
   NumberArray<NDIM, ADScalar> q = -k * T.derivatives();
 
   // Euler equation residuals
-  Scalar Q_rho = raw_value(divergence(RHO*U));
+  // Scalar Q_rho = raw_value(divergence(RHO*U));
   NumberArray<NDIM, Scalar> Q_rho_u = 
     raw_value(divergence(RHO*U.outerproduct(U) - Tau) + P.derivatives());
 
-  return U[1];
+  return Q_rho_u[1];
 
 }
 
 // public, static method
 double eval_q_e(double x, double y) const
 {
-  typedef typename RawType<ADScalar>::value_type Scalar;
+  typedef DualNumber<Scalar, NumberArray<NDIM, Scalar> > FirstDerivType;
+  typedef DualNumber<FirstDerivType, NumberArray<NDIM, FirstDerivType> > SecondDerivType;
+  typedef SecondDerivType ADScalar;
 
   // Treat velocity as a vector
   NumberArray<NDIM, ADScalar> U;
@@ -111,7 +116,7 @@ double eval_q_e(double x, double y) const
 
   // The identity tensor I
   NumberArray<NDIM, NumberArray<NDIM, Scalar> > Identity = 
-    NumberArrayIdentity<NDIM, Scalar>::identity();
+    NumberArray<NDIM, Scalar>::identity();
 
   // The shear stress tensor
   NumberArray<NDIM, NumberArray<NDIM, ADScalar> > Tau = mu * (GradU + transpose(GradU) - 2./3.*divergence(U)*Identity);
@@ -120,11 +125,9 @@ double eval_q_e(double x, double y) const
   NumberArray<NDIM, ADScalar> q = -k * T.derivatives();
 
   // Euler equation residuals
-  Scalar Q_rho = raw_value(divergence(RHO*U));
-  NumberArray<NDIM, Scalar> Q_rho_u = 
-    raw_value(divergence(RHO*U.outerproduct(U) - Tau) + P.derivatives());
-
-  Scalar Q_rho_e;
+  // Scalar Q_rho = raw_value(divergence(RHO*U));
+  // NumberArray<NDIM, Scalar> Q_rho_u = 
+  //   raw_value(divergence(RHO*U.outerproduct(U) - Tau) + P.derivatives());
 
   // energy equation
   Scalar Q_rho_e = raw_value(divergence((RHO*ET+P)*U + q - Tau.dot(U)));
@@ -133,16 +136,18 @@ double eval_q_e(double x, double y) const
 }
 
 // public, static method
-double eval_q_rho(double x1, double y1) const
+template <typename Scalar>
+Scalar MASA::ad_cns_2d_crossterms<Scalar>::eval_q_rho(Scalar x1, Scalar y1) const
 {
-  typedef typename RawType<ADScalar>::value_type Scalar;
+  typedef DualNumber<Scalar, NumberArray<NDIM, Scalar> > FirstDerivType;
+  typedef DualNumber<FirstDerivType, NumberArray<NDIM, FirstDerivType> > SecondDerivType;
+  typedef SecondDerivType ADScalar;
 
-  const ADScalar& x = x1;
-  const ADScalar& y = y1;
+  const Scalar& x = x1;
+  const Scalar& y = y1;
 
   // Treat velocity as a vector
   NumberArray<NDIM, ADScalar> U;
-
 
   // Arbitrary manufactured solution
   U[0] = u_0 + u_x * std::cos(a_ux * PI * x / L) * u_y * std::cos(a_uy * PI * y / L);
@@ -150,5 +155,6 @@ double eval_q_rho(double x1, double y1) const
   ADScalar RHO = rho_0 + rho_x * std::cos(a_rhox * PI * x / L) * rho_y * std::cos(a_rhoy * PI * y / L);
 
   Scalar Q_rho = raw_value(divergence(RHO*U));
-}
 
+  return Q_rho;
+}
