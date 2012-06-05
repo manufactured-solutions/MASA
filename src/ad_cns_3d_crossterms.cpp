48be1c4b2dbf981df0569583b62095c969b27e78
Nicholas Malaya <nicholas.malaya@gmail.com>
2012-06-05 14:04:34 +0000

    [psaap2]: adding cns ad file

---
+++ src/ad_cns_3d_crossterms.cpp
@@ -0,0 +1,400 @@
+// -*-c++-*-
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+
+#include <masa_internal.h>
+
+#include <ad_masa.h>
+
+typedef ShadowNumber<double, long double> RawScalar;
+const unsigned int NDIM = 3;
+typedef DualNumber<RawScalar, NumberArray<NDIM, RawScalar> > FirstDerivType;
+typedef DualNumber<FirstDerivType, NumberArray<NDIM, FirstDerivType> > SecondDerivType;
+typedef SecondDerivType ADType;
+
+using namespace MASA;
+
+template <typename Scalar>
+MASA::ad_cns_3d_crossterms<Scalar>::ad_cns_3d_crossterms()
+{
+  this->mmsname = "ad_cns_3d_crossterms";
+  this->dimension = 3;
+
+  this->register_var("R",&R);
+  this->register_var("k",&k);
+  this->register_var("u_0",&u_0);
+  this->register_var("u_x",&u_x);
+  this->register_var("u_y",&u_y);
+  this->register_var("u_z",&u_z);
+  this->register_var("v_0",&v_0);
+  this->register_var("v_x",&v_x);
+  this->register_var("v_y",&v_y);
+  this->register_var("v_z",&v_z);
+  this->register_var("w_0",&w_0);
+  this->register_var("w_x",&w_x);
+  this->register_var("w_y",&w_y);
+  this->register_var("w_z",&w_z);
+  this->register_var("rho_0",&rho_0);
+  this->register_var("rho_x",&rho_x);
+  this->register_var("rho_y",&rho_y);
+  this->register_var("rho_z",&rho_z);
+  this->register_var("p_0",&p_0);
+  this->register_var("p_x",&p_x);
+  this->register_var("p_y",&p_y);
+  this->register_var("p_z",&p_z);
+  this->register_var("a_px",&a_px);
+  this->register_var("a_py",&a_py);
+  this->register_var("a_pz",&a_pz);
+  this->register_var("a_rhox",&a_rhox);
+  this->register_var("a_rhoy",&a_rhoy);
+  this->register_var("a_rhoz",&a_rhoz);
+  this->register_var("a_ux",&a_ux);
+  this->register_var("a_uy",&a_uy);
+  this->register_var("a_uz",&a_uz);
+  this->register_var("a_vx",&a_vx);
+  this->register_var("a_vy",&a_vy);
+  this->register_var("a_vz",&a_vz);
+  this->register_var("a_wx",&a_wx);
+  this->register_var("a_wy",&a_wy);
+  this->register_var("a_wz",&a_wz);
+  this->register_var("Gamma",&Gamma);
+  this->register_var("mu",&mu);
+  this->register_var("L",&L);
+
+  this->init_var();
+
+} // done with constructor
+
+template <typename Scalar>
+int MASA::ad_cns_3d_crossterms<Scalar>::init_var()
+{
+  int err = 0;
+
+  err += this->set_var("R",1.01);
+  err += this->set_var("k",1.38);
+  err += this->set_var("u_0",10.23);
+  err += this->set_var("u_x",1.1);
+  err += this->set_var("u_y",.08);
+  err += this->set_var("u_z",.08);
+  err += this->set_var("v_0",12);
+  err += this->set_var("v_x",1.6);
+  err += this->set_var("v_y",.67);
+  err += this->set_var("v_z",.67);
+  err += this->set_var("w_0",12);
+  err += this->set_var("w_x",1.6);
+  err += this->set_var("w_y",.67);
+  err += this->set_var("w_z",.67);
+  err += this->set_var("rho_0",10.02);
+  err += this->set_var("rho_x",7.2);
+  err += this->set_var("rho_y",9.8);
+  err += this->set_var("rho_z",9.8);
+  err += this->set_var("p_0",10.2);
+  err += this->set_var("p_x",.91);
+  err += this->set_var("p_y",.623);
+  err += this->set_var("p_z",.623);
+  err += this->set_var("a_px",.165);
+  err += this->set_var("a_py",.612);
+  err += this->set_var("a_pz",.612);
+  err += this->set_var("a_rhox",.627);
+  err += this->set_var("a_rhoy",.828);
+  err += this->set_var("a_rhoz",.828);
+  err += this->set_var("a_ux",.1987);
+  err += this->set_var("a_uy",1.189);
+  err += this->set_var("a_uz",1.189);
+  err += this->set_var("a_vx",1.91);
+  err += this->set_var("a_vy",2.901);
+  err += this->set_var("a_vz",2.901);
+  err += this->set_var("a_wx",1.91);
+  err += this->set_var("a_wy",2.901);
+  err += this->set_var("a_wz",2.901);
+  err += this->set_var("Gamma",1.01);
+  err += this->set_var("mu",.918);
+  err += this->set_var("L",3.02);
+
+  return err;
+
+} // done with init_var
+
+// ----------------------------------------
+// Source Terms
+// ----------------------------------------
+
+// public static method, that can be called from eval_q_t
+template <typename Scalar>
+Scalar MASA::ad_cns_3d_crossterms<Scalar>::eval_q_u(Scalar x, Scalar y, Scalar z) const
+{
+  typedef DualNumber<Scalar, NumberArray<NDIM, Scalar> > FirstDerivType;
+  typedef DualNumber<FirstDerivType, NumberArray<NDIM, FirstDerivType> > SecondDerivType;
+  typedef SecondDerivType ADScalar;
+
+  // Treat velocity as a vector
+  NumberArray<NDIM, ADScalar> U;
+
+  // Arbitrary manufactured solution
+  U[0] = u_0 + u_x * std::cos(a_ux * PI * x / L) * u_y * std::cos(a_uy * PI * y / L) * std::cos(a_uy * PI * z / L);
+  U[1] = v_0 + v_x * std::cos(a_vx * PI * x / L) * v_y * std::cos(a_vy * PI * y / L) * std::cos(a_vy * PI * z / L);
+  U[2] = w_0 + w_x * std::cos(a_wx * PI * x / L) * w_y * std::cos(a_wy * PI * y / L) * std::cos(a_wy * PI * z / L);
+  ADScalar RHO = rho_0 + rho_x * std::cos(a_rhox * PI * x / L) * rho_y * std::cos(a_rhoy * PI * y / L) * std::cos(a_rhoz * PI * z / L);
+  ADScalar P = p_0 + p_x * std::cos(a_px * PI * x / L) * p_y * std::cos(a_py * PI * y / L) * std::cos(a_pz * PI * z / L);
+
+  // Temperature
+  ADScalar T = P / RHO / R;
+
+  // Perfect gas energies
+  ADScalar E = 1./(Gamma-1.)*P/RHO;
+  ADScalar ET = E + .5 * U.dot(U);
+
+  // The shear strain tensor
+  NumberArray<NDIM, typename ADScalar::derivatives_type> GradU = gradient(U);
+
+  // The identity tensor I
+  NumberArray<NDIM, NumberArray<NDIM, Scalar> > Identity = 
+    NumberArray<NDIM, Scalar>::identity();
+
+  // The shear stress tensor
+  NumberArray<NDIM, NumberArray<NDIM, ADScalar> > Tau = mu * (GradU + transpose(GradU) - 2./3.*divergence(U)*Identity);
+
+  // Temperature flux
+  NumberArray<NDIM, ADScalar> q = -k * T.derivatives();
+
+  // Euler equation residuals
+  // Scalar Q_rho = raw_value(divergence(RHO*U));
+  NumberArray<NDIM, Scalar> Q_rho_u = 
+    raw_value(divergence(RHO*U.outerproduct(U) - Tau) + P.derivatives());
+
+  return Q_rho_u[0];
+}
+
+// public, static method
+template <typename Scalar>
+Scalar MASA::ad_cns_3d_crossterms<Scalar>::eval_q_v(Scalar x, Scalar y, Scalar z) const
+{
+  typedef DualNumber<Scalar, NumberArray<NDIM, Scalar> > FirstDerivType;
+  typedef DualNumber<FirstDerivType, NumberArray<NDIM, FirstDerivType> > SecondDerivType;
+  typedef SecondDerivType ADScalar;
+
+  // Treat velocity as a vector
+  NumberArray<NDIM, ADScalar> U;
+
+  // Arbitrary manufactured solution
+  U[0] = u_0 + u_x * std::cos(a_ux * PI * x / L) * u_y * std::cos(a_uy * PI * y / L) * std::cos(a_uy * PI * z / L);
+  U[1] = v_0 + v_x * std::cos(a_vx * PI * x / L) * v_y * std::cos(a_vy * PI * y / L) * std::cos(a_vy * PI * z / L);
+  U[2] = w_0 + w_x * std::cos(a_wx * PI * x / L) * w_y * std::cos(a_wy * PI * y / L) * std::cos(a_wy * PI * z / L);
+  ADScalar RHO = rho_0 + rho_x * std::cos(a_rhox * PI * x / L) * rho_y * std::cos(a_rhoy * PI * y / L) * std::cos(a_rhoz * PI * z / L);
+  ADScalar P = p_0 + p_x * std::cos(a_px * PI * x / L) * p_y * std::cos(a_py * PI * y / L) * std::cos(a_pz * PI * z / L);
+
+  // Temperature
+  ADScalar T = P / RHO / R;
+
+ // Perfect gas energies
+  ADScalar E = 1./(Gamma-1.)*P/RHO;
+  ADScalar ET = E + .5 * U.dot(U);
+
+  // The shear strain tensor
+  NumberArray<NDIM, typename ADScalar::derivatives_type> GradU = gradient(U);
+
+  // The identity tensor I
+  NumberArray<NDIM, NumberArray<NDIM, Scalar> > Identity = 
+    NumberArray<NDIM, Scalar>::identity();
+
+  // The shear stress tensor
+  NumberArray<NDIM, NumberArray<NDIM, ADScalar> > Tau = mu * (GradU + transpose(GradU) - 2./3.*divergence(U)*Identity);
+
+  // Temperature flux
+  NumberArray<NDIM, ADScalar> q = -k * T.derivatives();
+
+  // Euler equation residuals
+  // Scalar Q_rho = raw_value(divergence(RHO*U));
+  NumberArray<NDIM, Scalar> Q_rho_u = 
+    raw_value(divergence(RHO*U.outerproduct(U) - Tau) + P.derivatives());
+
+  return Q_rho_u[1];
+
+}
+
+// public, static method
+template <typename Scalar>
+Scalar MASA::ad_cns_3d_crossterms<Scalar>::eval_q_w(Scalar x, Scalar y, Scalar z) const
+{
+  typedef DualNumber<Scalar, NumberArray<NDIM, Scalar> > FirstDerivType;
+  typedef DualNumber<FirstDerivType, NumberArray<NDIM, FirstDerivType> > SecondDerivType;
+  typedef SecondDerivType ADScalar;
+
+  // Treat velocity as a vector
+  NumberArray<NDIM, ADScalar> U;
+
+  // Arbitrary manufactured solution
+  U[0] = u_0 + u_x * std::cos(a_ux * PI * x / L) * u_y * std::cos(a_uy * PI * y / L) * std::cos(a_uy * PI * z / L);
+  U[1] = v_0 + v_x * std::cos(a_vx * PI * x / L) * v_y * std::cos(a_vy * PI * y / L) * std::cos(a_vy * PI * z / L);
+  U[2] = w_0 + w_x * std::cos(a_wx * PI * x / L) * w_y * std::cos(a_wy * PI * y / L) * std::cos(a_wy * PI * z / L);
+  ADScalar RHO = rho_0 + rho_x * std::cos(a_rhox * PI * x / L) * rho_y * std::cos(a_rhoy * PI * y / L) * std::cos(a_rhoz * PI * z / L);
+  ADScalar P = p_0 + p_x * std::cos(a_px * PI * x / L) * p_y * std::cos(a_py * PI * y / L) * std::cos(a_pz * PI * z / L);
+
+  // Temperature
+  ADScalar T = P / RHO / R;
+
+ // Perfect gas energies
+  ADScalar E = 1./(Gamma-1.)*P/RHO;
+  ADScalar ET = E + .5 * U.dot(U);
+
+  // The shear strain tensor
+  NumberArray<NDIM, typename ADScalar::derivatives_type> GradU = gradient(U);
+
+  // The identity tensor I
+  NumberArray<NDIM, NumberArray<NDIM, Scalar> > Identity = 
+    NumberArray<NDIM, Scalar>::identity();
+
+  // The shear stress tensor
+  NumberArray<NDIM, NumberArray<NDIM, ADScalar> > Tau = mu * (GradU + transpose(GradU) - 2./3.*divergence(U)*Identity);
+
+  // Temperature flux
+  NumberArray<NDIM, ADScalar> q = -k * T.derivatives();
+
+  // Euler equation residuals
+  // Scalar Q_rho = raw_value(divergence(RHO*U));
+  NumberArray<NDIM, Scalar> Q_rho_u = 
+    raw_value(divergence(RHO*U.outerproduct(U) - Tau) + P.derivatives());
+
+  return Q_rho_u[2];
+
+}
+
+// public, static method
+template <typename Scalar>
+Scalar MASA::ad_cns_3d_crossterms<Scalar>::eval_q_e(Scalar x, Scalar y, Scalar z) const
+{
+  typedef DualNumber<Scalar, NumberArray<NDIM, Scalar> > FirstDerivType;
+  typedef DualNumber<FirstDerivType, NumberArray<NDIM, FirstDerivType> > SecondDerivType;
+  typedef SecondDerivType ADScalar;
+
+  // Treat velocity as a vector
+  NumberArray<NDIM, ADScalar> U;
+
+  // Arbitrary manufactured solution
+  U[0] = u_0 + u_x * std::cos(a_ux * PI * x / L) * u_y * std::cos(a_uy * PI * y / L) * std::cos(a_uy * PI * z / L);
+  U[1] = v_0 + v_x * std::cos(a_vx * PI * x / L) * v_y * std::cos(a_vy * PI * y / L) * std::cos(a_vy * PI * z / L);
+  U[2] = w_0 + w_x * std::cos(a_wx * PI * x / L) * w_y * std::cos(a_wy * PI * y / L) * std::cos(a_wy * PI * z / L);
+  ADScalar RHO = rho_0 + rho_x * std::cos(a_rhox * PI * x / L) * rho_y * std::cos(a_rhoy * PI * y / L) * std::cos(a_rhoz * PI * z / L);
+  ADScalar P = p_0 + p_x * std::cos(a_px * PI * x / L) * p_y * std::cos(a_py * PI * y / L) * std::cos(a_pz * PI * z / L);
+
+  // Temperature
+  ADScalar T = P / RHO / R;
+
+  // Perfect gas energies
+  ADScalar E = 1./(Gamma-1.)*P/RHO;
+  ADScalar ET = E + .5 * U.dot(U);
+
+  // The shear strain tensor
+  NumberArray<NDIM, typename ADScalar::derivatives_type> GradU = gradient(U);
+
+  // The identity tensor I
+  NumberArray<NDIM, NumberArray<NDIM, Scalar> > Identity = 
+    NumberArray<NDIM, Scalar>::identity();
+
+  // The shear stress tensor
+  NumberArray<NDIM, NumberArray<NDIM, ADScalar> > Tau = mu * (GradU + transpose(GradU) - 2./3.*divergence(U)*Identity);
+
+  // Temperature flux
+  NumberArray<NDIM, ADScalar> q = -k * T.derivatives();
+
+  // Euler equation residuals
+  // Scalar Q_rho = raw_value(divergence(RHO*U));
+  // NumberArray<NDIM, Scalar> Q_rho_u = 
+  //   raw_value(divergence(RHO*U.outerproduct(U) - Tau) + P.derivatives());
+
+  // energy equation
+  Scalar Q_rho_e = raw_value(divergence((RHO*ET+P)*U + q - Tau.dot(U)));
+
+  return Q_rho_e;
+}
+
+
+// public, static method
+template <typename Scalar>
+Scalar MASA::ad_cns_3d_crossterms<Scalar>::eval_q_rho(Scalar x1, Scalar y1, Scalar z1) const
+{
+  typedef DualNumber<Scalar, NumberArray<NDIM, Scalar> > FirstDerivType;
+  typedef DualNumber<FirstDerivType, NumberArray<NDIM, FirstDerivType> > SecondDerivType;
+  typedef SecondDerivType ADScalar;
+
+  const Scalar& x = x1;
+  const Scalar& y = y1;
+  const Scalar& z = z1;
+
+  // Treat velocity as a vector
+  NumberArray<NDIM, ADScalar> U;
+
+  // Arbitrary manufactured solution
+  U[0] = u_0 + u_x * std::cos(a_ux * PI * x / L) * u_y * std::cos(a_uy * PI * y / L) * std::cos(a_uy * PI * z / L);
+  U[1] = v_0 + v_x * std::cos(a_vx * PI * x / L) * v_y * std::cos(a_vy * PI * y / L) * std::cos(a_vy * PI * z / L);
+  U[2] = w_0 + w_x * std::cos(a_wx * PI * x / L) * w_y * std::cos(a_wy * PI * y / L) * std::cos(a_wy * PI * z / L);
+  ADScalar RHO = rho_0 + rho_x * std::cos(a_rhox * PI * x / L) * rho_y * std::cos(a_rhoy * PI * y / L) * std::cos(a_rhoz * PI * z / L);
+  ADScalar P = p_0 + p_x * std::cos(a_px * PI * x / L) * p_y * std::cos(a_py * PI * y / L) * std::cos(a_pz * PI * z / L);
+
+  Scalar Q_rho = raw_value(divergence(RHO*U));
+
+  return Q_rho;
+}
+
+
+// ----------------------------------------
+// Analytical Terms
+// ----------------------------------------
+
+// example of a public method called from eval_exact_t
+template <typename Scalar>
+Scalar MASA::ad_cns_3d_crossterms<Scalar>::eval_exact_u(Scalar x, Scalar y, Scalar z)
+{
+  Scalar exact_u;
+  exact_u = u_0 + u_x * std::cos(a_ux * PI * x / L) * u_y * std::cos(a_uy * PI * y / L) * std::cos(a_uz * PI * z / L);
+  return exact_u;
+}
+
+// public method
+template <typename Scalar>
+Scalar MASA::ad_cns_3d_crossterms<Scalar>::eval_exact_v(Scalar x, Scalar y, Scalar z)
+{
+  Scalar exact_v;
+  exact_v = v_0 + v_x * std::cos(a_vx * PI * x / L) * v_y * std::cos(a_vy * PI * y / L) * std::cos(a_vz * PI * z / L);
+  return exact_v;
+}
+
+// public method
+template <typename Scalar>
+Scalar MASA::ad_cns_3d_crossterms<Scalar>::eval_exact_w(Scalar x, Scalar y, Scalar z)
+{
+  Scalar exact_v;
+  exact_v = v_0 + v_x * std::cos(a_vx * PI * x / L) * v_y * std::cos(a_vy * PI * y / L) * std::cos(a_wz * PI * z / L);
+  return exact_v;
+}
+
+// public method
+template <typename Scalar>
+Scalar MASA::ad_cns_3d_crossterms<Scalar>::eval_exact_p(Scalar x, Scalar y, Scalar z)
+{
+  Scalar P = p_0 + p_x * std::cos(a_px * PI * x / L) * p_y * std::cos(a_py * PI * y / L) * std::cos(a_pz * PI * z / L);
+  return P;
+}
+
+// public method
+template <typename Scalar>
+Scalar MASA::ad_cns_3d_crossterms<Scalar>::eval_exact_rho(Scalar x, Scalar y, Scalar z)
+{
+  Scalar RHO = rho_0 + rho_x * std::cos(a_rhox * PI * x / L) * rho_y * std::cos(a_rhoy * PI * y / L) * std::cos(a_rhoz * PI * z / L);
+  return RHO;
+}
+
+
+
+// ----------------------------------------
+// Template Instantiation(s)
+// ----------------------------------------
+
+MASA_INSTANTIATE_ALL(MASA::ad_cns_3d_crossterms);
+
+
+
+//---------------------------------------------------------
+// AUTOMASA
+// Generated on: 2012-05-31 13:55:08
+//---------------------------------------------------------
