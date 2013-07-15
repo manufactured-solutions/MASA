6e5f78c91d77ba4a1f0da45432e2b31210cbee21
Nicholas Malaya <nicholas.malaya@gmail.com>
2013-07-15 19:00:02 +0000

    [masa]: at least a primitive python test now exists

--- tests/test_swig.py
+++ tests/test_swig.py
@@ -2,13 +2,29 @@
 #
 # touch swig interfaces, ensure they function
 #
-import sys
-#import masa
 
+#
+# import .libs relative path for masa.py
+#
+import os, sys
+lib_path = os.path.abspath('../src/.libs')
+sys.path.append(lib_path)
+import _masa
 
-# Has the whole world gone crazy? 
-# MARK IT ZERO
-sys.exit(0)
+err  = 0
+err += _masa.masa_init("heat equation example","heateq_2d_steady_const")
+err += _masa.masa_sanity_check()
+
+tfield = _masa.masa_eval_2d_source_t(0.1,0.2)
+
+if(err == 0):
+    # Has the whole world gone crazy? 
+    # MARK IT ZERO
+    sys.exit(0)
+else:
+    # something is wrong
+    sys.exit(1)
+
 
 #
 # nick
