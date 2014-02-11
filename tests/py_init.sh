b770e19a74e6556e64488da6ef5c17b253fe0842
Roy H. Stogner <roystgnr@ices.utexas.edu>
2014-02-11 16:28:17 -0600

    These got replaced by configure-generated files

--- tests/py_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo " "
-echo "-------------------------------------------------------"
-echo "Initializing pythonMASA Tests"
-echo "-------------------------------------------------------"
-
-python test_swig.py
